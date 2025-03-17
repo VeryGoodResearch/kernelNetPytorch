5ce3d02cee9e0597a811e8802b0c616a104b55d0
Kasia Osowska <kasia@osowski.info>
2025-03-17 13:03:10 +0100

    add loading ratings with user's personality traits

--- train_personality_traits.py
+++ train_personality_traits.py
@@ -3,23 +3,27 @@ import torch
 import time
 from kernelNet.training_runner import train_model
 from kernelNet.kernel import gaussian_kernel
-from dataLoader.dataLoader import load_movie_lens, load_jester_data_xls
+
+from dataLoader.dataLoader import load_ratings_with_personality_traits
 
 
 def main():
     seed = int(time.time())
     torch.seed()
-    # In case transpose=False each sample represents a user
-    train_data, validation_data = load_movie_lens('../ml-1m/ratings.dat', delimiter='::', transpose=True, valfrac=0.1, seed=seed)
+    train_data, validation_data, train_user_features, valid_user_features = load_ratings_with_personality_traits(
+        path='../personality-isf2018/', valfrac=0.1, seed=seed, transpose=False)
+
     train_data = torch.from_numpy(train_data)
     validation_data = torch.from_numpy(validation_data)
+    train_user_features = torch.from_numpy(train_user_features)
+    valid_user_features = torch.from_numpy(valid_user_features)
     train_mask = torch.greater_equal(train_data, 1).float()
     validation_mask = torch.greater_equal(validation_data, 1).float()
     print(f'Training shape: {train_data.shape}, validation shape: {validation_data.shape}')
     print(f'Training mask: {train_mask.shape}, validation mask shape: {validation_mask.shape}')
 
     output_every=50
-    epochs = output_every * 30 
+    epochs = output_every * 30
     model = train_model(
             epochs,
             train_data,
@@ -33,7 +37,7 @@ def main():
             history_size=10,
             output_every=50,
             hidden_dims=50,
-            output_path='./output/',
+            output_path='./output_personality/',
             min_rating=1.0,
             max_rating=5.0)
     print(model)
