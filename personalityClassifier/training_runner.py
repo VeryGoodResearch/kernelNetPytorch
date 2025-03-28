c0e9fd063fe6fc552769f85111539660a7c5af96
tpalinski <tpalinski@icloud.com>
2025-03-28 12:53:30 +0100

    added zabson lyrics

---
+++ personalityClassifier/training_runner.py
@@ -0,0 +1,133 @@
+# This module provides some helper utilities used for training the model
+from datetime import datetime
+from os import path, makedirs
+from numpy.strings import upper
+import torch
+import numpy as np
+import time
+from torchmin import ScipyMinimizer
+
+from personalityClassifier.autoencoder import KernelNetAutoencoder
+from personalityClassifier.kernel import gaussian_kernel
+
+def _loss(predictions: torch.Tensor, 
+          truth: torch.Tensor, 
+          reg_term: torch.Tensor,
+          mask: torch.Tensor):
+    masked_diff = mask * (truth - predictions)
+    loss = torch.sum(masked_diff**2) / 2
+    loss = loss + reg_term 
+    return loss
+
+
+def _training_iter(model: KernelNetAutoencoder, 
+                  epoch: int,
+                  t_data: torch.Tensor, 
+                  v_data: torch.Tensor,
+                  t_mask: torch.Tensor,
+                  v_mask: torch.Tensor,
+                  optimizer: torch.optim.Optimizer,
+                  log_file,
+                  min_rating: float,
+                  max_rating: float):
+    # Not the greatest idea to run it otherwise
+    assert torch.is_grad_enabled()
+    def optimizer_run():
+        optimizer.zero_grad()
+        t_pred, t_reg = model.forward(t_data)
+        loss = _loss(t_pred, t_data, t_reg, t_mask)
+        loss.backward()
+        return loss
+
+    optimizer.step(optimizer_run)
+
+    # Validation
+    with torch.no_grad():
+        predictions, t_reg = model.forward(t_data)
+        clipped = torch.clamp(predictions, 1.0, 5.0)
+        error_validation = (v_mask * (clipped - v_data) ** 2).sum() / v_mask.sum()  # compute validation error
+        error_train = (t_mask * (clipped - t_data) ** 2).sum() / t_mask.sum()  # compute train error
+        loss_train = _loss(predictions, t_data, t_reg, t_mask)
+        loss_validation = _loss(predictions, v_data, t_reg, v_mask)
+
+        print('.-^-._' * 12, file=log_file)
+        print('epoch:', epoch, file=log_file) 
+        print('validation rmse:', np.sqrt(error_validation), 'train rmse:', np.sqrt(error_train), file=log_file)
+        print('validation loss: ', loss_validation, ', train_loss: ', loss_train, file=log_file)
+        print('.-^-._' * 12, file=log_file)
+
+        print('.-^-._' * 12) 
+        print('epoch:', epoch) 
+        print('validation rmse:', np.sqrt(error_validation), 'train rmse:', np.sqrt(error_train))
+        print('validation loss: ', loss_validation, ', train_loss: ', loss_train)
+        print('.-^-._' * 12)
+
+def train_model(
+        epochs: int,
+        training_data: torch.Tensor,
+        validation_data: torch.Tensor,
+        training_mask: torch.Tensor,
+        validation_mask: torch.Tensor,
+        min_rating: float,
+        max_rating: float,
+        output_path: str, 
+        lambda_o: float = 0.013,
+        lambda_2: float = 60,
+        activation = torch.sigmoid_,
+        kernel = gaussian_kernel,
+        hidden_dims = 500,
+        output_every: int = 5,
+        # WARNING WARNING WARNING 
+        # IF YOU HAVE BAD PC (LOW MEMORY) THEN TUNE THIS THING DOWN, OTHERWISE IT WILL PROBABLY EXPLODE
+        history_size: int = 10,
+        learning_rate: float = 1
+        ):
+    n_input = training_data.shape[1]
+    model = KernelNetAutoencoder(
+            n_input,
+            lambda_o=lambda_o,
+            lambda_2=lambda_2,
+            kernel_hidden=hidden_dims,
+            kernel_function=kernel,
+            activation=activation,
+            )
+    """
+    optimizer = torch.optim.LBFGS(
+             model.parameters(), 
+             max_iter=output_every, 
+             history_size=history_size,
+             lr=learning_rate,
+             line_search_fn='strong_wolfe'
+             )
+    optimizer = torch.optim.Rprop(
+           model.parameters(),
+           lr=learning_rate
+           )
+    """
+    optimizer = ScipyMinimizer(
+            model.parameters(),
+            method='L-BFGS-B',
+            options={'maxiter': output_every, 'disp': True, 'maxcor': history_size}
+            )
+    n_epochs = int(epochs/output_every)
+    makedirs(output_path, exist_ok=True)
+    log_path= path.join(output_path, f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log')
+    with open(log_path, 'w+') as log_file:
+        for epoch in range(n_epochs):
+            start = time.time()
+            _training_iter(
+                    model,
+                    epoch,
+                    training_data,
+                    validation_data,
+                    training_mask,
+                    validation_mask,
+                    optimizer,
+                    log_file,
+                    min_rating,
+                    max_rating
+                    )
+            elapsed = time.time() - start
+            # save_model(model, output_path)
+            print(f'Run took {elapsed} seconds')
+    return model
---
+++ train_classifier_autoencoder.py
@@ -0,0 +1,56 @@
+# Some testing stuff for model
+import torch
+import time
+from kernelNet.training_runner_for_combined_model import train_model
+from kernelNet.training_runner import train_model as original_train
+
+from kernelNet.kernel import gaussian_kernel
+
+from dataLoader.dataLoader import load_ratings_with_personality_traits
+
+
+def main():
+    seed = int(time.time())
+    torch.seed()
+    train_data, validation_data, train_user_features, valid_user_features = load_ratings_with_personality_traits(
+        path='../personality-isf2018/', valfrac=0.1, seed=seed, transpose=False)
+
+    train_data = torch.from_numpy(train_data)
+    validation_data = torch.from_numpy(validation_data)
+    train_user_features = torch.from_numpy(train_user_features)
+    n_users = train_user_features.shape[0]
+    n_user_features = train_user_features.shape[1]
+    valid_user_features = torch.from_numpy(valid_user_features)
+    train_mask = torch.greater_equal(train_data, 1).float()
+    validation_mask = torch.greater_equal(validation_data, 1).float()
+    print(f'Training shape: {train_data.shape}, validation shape: {validation_data.shape}')
+    print(f'Training mask: {train_mask.shape}, validation mask shape: {validation_mask.shape}')
+
+    output_every=50
+    epochs = output_every * 30
+    model = train_model(
+            epochs,
+            train_data,
+            train_user_features,
+            validation_data,
+            valid_user_features,
+            train_mask,
+            validation_mask,
+            n_user_features,
+            n_users,
+            kernel=gaussian_kernel,
+            activation=torch.nn.Sigmoid(),
+            lambda_o=0.013,
+            lambda_2=60,
+            history_size=10,
+            output_every=50,
+            hidden_dims=50,
+            output_path='./output_personality/',
+            min_rating=1.0,
+            max_rating=5.0,
+            user_features_weight = 0.5)
+    print(model)
+
+if __name__ == '__main__':
+    main()
+    #train_without_personality_traits()
