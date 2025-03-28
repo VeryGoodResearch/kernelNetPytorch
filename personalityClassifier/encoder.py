bbfb1304e238c310dc52f2bd2479aa5f647a5492
tpalinski <tpalinski@icloud.com>
2025-03-28 12:23:08 +0100

    Autoencoder for classifier model definition

---
+++ personalityClassifier/encoder.py
@@ -0,0 +1,49 @@
+# Definition of the encoder part of the autoencoder
+from torch import nn
+
+from personalityClassifier.kernel_layer import KernelLayer
+from personalityClassifier.utils import get_device
+from .kernel import gaussian_kernel
+
+class Encoder(nn.Module):
+    
+    def __init__(self,
+                 n_input, # Input/output dims
+                 kernel_hidden = 500, # Kernel layer hidden dimensions
+                 lambda_o: float = 0.013,
+                 lambda_2: float = 60,
+                 hidden_dims = 5,
+                 kernel_function = gaussian_kernel,
+                 activation = torch.sigmoid
+                 ) -> None:
+        super().__init__()
+        self.device = get_device()
+        self.layers = nn.Sequential(
+                KernelLayer(n_in=n_input,
+                            activation=activation,
+                            n_hid=kernel_hidden,
+                            n_dim=hidden_dims,
+                            lambda_o=lambda_o,
+                            lambda_2=lambda_2,
+                            kernel=kernel_function
+                            ),
+                KernelLayer(n_in=kernel_hidden,
+                            activation=activation,
+                            n_hid=kernel_hidden,
+                            n_dim=hidden_dims,
+                            lambda_o=lambda_o,
+                            lambda_2=lambda_2,
+                            kernel=kernel_function
+                            )
+                ).to(self.device)
+
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        total_reg = None
+        y = x
+        for layer in self.layers.children():
+            y, current_reg = layer.forward(y)
+            total_reg = current_reg if total_reg is None else total_reg+current_reg
+        return y, total_reg
+
+    def parameters(self, recurse: bool = True):
+        return self.layers.parameters(recurse)
---
+++ personalityClassifier/utils.py
@@ -0,0 +1,2 @@
+def get_device():
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
