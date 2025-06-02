e6f68d2eff3477d24bdf2c4e6c03e0500a7d2361
tpalinski <tpalinski@icloud.com>
2025-06-02 15:23:33 +0200

    add weighted ndcg

--- personalityClassifier/utils.py
+++ personalityClassifier/utils.py
@@ -5,13 +5,13 @@ from sklearn.metrics import ndcg_score
 def get_device():
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def _compute_ndcg_from_indices(true_items, predicted_items, k=5, num_items=None):
+def _compute_ndcg_from_indices(true_items, predicted_items, true_ratings, k=5, num_items=None):
     if num_items is None:
         num_items = max(max(true_items, default=0), max(predicted_items, default=0)) + 1
 
     y_true = np.zeros(num_items)
-    for item in true_items:
-        y_true[item] = 1
+    for idx, item in enumerate(true_items):
+        y_true[item] = true_ratings[idx]
 
     y_score = np.zeros(num_items)
     for rank, item in enumerate(predicted_items[:k]):
@@ -21,13 +21,14 @@ def _compute_ndcg_from_indices(true_items, predicted_items, k=5, num_items=None)
 
 def evaluate_reccomendation_list(X: np.ndarray, X_hat: np.ndarray, n=20, threshold=2.0):
     top_true = [np.where(user_ratings > threshold)[0] for user_ratings in X]
+    true_ratings = [X[idx][r] for idx, r in enumerate(top_true)]
     top_predicted = np.argsort(X_hat, axis=1, stable=True)[:,::-1][:, :n]
-    return top_true, top_predicted
+    return top_true, top_predicted, true_ratings
 
-def compute_ndcg(X_true, X_pred, k, num_items):
+def compute_ndcg(X_true, X_pred, true_ratings, k, num_items):
     scores = np.zeros((len(X_true)))
     for idx, user in enumerate(X_true):
-        scores[idx] = _compute_ndcg_from_indices(user, X_pred[idx], k=k, num_items=num_items)
+        scores[idx] = _compute_ndcg_from_indices(user, X_pred[idx], true_ratings[idx], k=k, num_items=num_items)
     return scores.mean()
 
 
