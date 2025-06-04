fix data leak

dataLoader/dataLoader.py
@@ -159,6 +159,7 @@ def load_ratings_with_personality_traits(path='./', valfrac=0.1, seed=1234, feat
        - `val_user_features` (numpy.ndarray): Validation set containing personality traits.
        """
     np.random.seed(seed)
+    rng = np.random.default_rng(seed)
 
     df_personality = pd.read_csv(path + "/personality-data.csv")
     df_personality.columns = df_personality.columns.str.strip()
@@ -187,7 +188,7 @@ def load_ratings_with_personality_traits(path='./', valfrac=0.1, seed=1234, feat
 
     i = 0
     data = []
-    for user in common_users:
+    for user in sorted(common_users):
 
         if user not in df_pivot["user_id"].values or user not in df_personality["user_id"].values:
             continue
@@ -210,7 +211,7 @@ def load_ratings_with_personality_traits(path='./', valfrac=0.1, seed=1234, feat
     ratings = np.array([user["ratings"] for user in data], dtype=np.float32).squeeze()
     user_features = np.array([user["features"] for user in data], dtype=np.float32)
 
-    np.random.shuffle(data)
+    rng.shuffle(data)
     num_val = int(len(data) * valfrac)
 
     train_user_features = user_features[num_val:]
@@ -290,7 +291,7 @@ def load_top_movies_with_personality_traits(path='./', valfrac=0.1, seed=1234, f
     p_test = p_test[np.where(np.count_nonzero(X_test, axis=1)>0)]
     return X_train, X_test, p_train, p_test, top_indices
 
-def load_mid_movies_with_personality_traits(path='./', valfrac=0.1, seed=1234, feature_classification = False, transpose=False, n=500, cutoff=10000):
+def load_mid_movies_with_personality_traits(path='./', valfrac=0.1, seed=1234, feature_classification = False, transpose=False, n=500, cutoff=2000):
     X_train, X_test, p_train, p_test = load_ratings_with_personality_traits(path, valfrac, seed, feature_classification, transpose)
     movie_ratings = np.count_nonzero(X_train, axis=0) + np.count_nonzero(X_test, axis=0)
     movie_ratings = movie_ratings.ravel()

ensemble/ensemble_model.py
@@ -44,54 +44,19 @@ class EnsembleModel(nn.Module):
         top_indices = torch.argsort(ratings, descending=True, dim=1)[:,:self.k]
         return top_indices
 
-    def __fuse_rec_lists(self, top: torch.Tensor, mid: torch.Tensor, sim: torch.Tensor, probs: torch.Tensor):
-        out = torch.empty(self.k, dtype=top.dtype)
-        filled = 0
-        all_common = top[torch.isin(top, mid) & torch.isin(top, sim) & torch.isin(mid, sim)]
-        for e in all_common:
-            out[filled] = e
-            filled += 1
-            if filled >= self.k:
-                return out
-
-        tm = top[torch.isin(top, mid) & ~torch.isin(top, all_common)]
-        for e in tm:
-            out[filled] = e
-            filled += 1
-            if filled >= self.k:
-                return out
-
-        ts = top[torch.isin(top, sim) & ~torch.isin(top, all_common)]
-        for e in ts:
-            out[filled] = e
-            filled += 1
-            if filled >= self.k:
-                return out
-
-        ms = mid[torch.isin(mid, sim) & ~torch.isin(mid, all_common)]
-        for e in ms:
-            out[filled] = e
-            filled += 1
-            if filled >= self.k:
-                return out
-
-        top_mask = ~torch.isin(top, all_common) & ~torch.isin(top, tm) & ~torch.isin(top, ts)
-        mid_mask = ~torch.isin(mid, all_common) & ~torch.isin(mid, tm) & ~torch.isin(mid, ms)
-        sim_mask = ~torch.isin(sim, all_common) & ~torch.isin(sim, ts) & ~torch.isin(sim, ms)
-
-        top_pool = top[top_mask]
-        mid_pool = mid[mid_mask]
-        sim_pool = sim[sim_mask]
-        pools = [top_pool, mid_pool, sim_pool]
-
-        for _ in range(self.k - filled):
-            pool_idx = torch.multinomial(probs, 1).item()
-            e = pools[pool_idx][0]
-            pools[pool_idx] = pools[pool_idx][1:pools[pool_idx].shape[0]]
-            out[filled] = e
-            filled += 1
-
-        return out
+    def __fuse_rec_lists(self, top: torch.Tensor, mid: torch.Tensor, sim: torch.Tensor, top_rates: torch.Tensor, mid_rates: torch.Tensor, sim_rates: torch.Tensor, probs: torch.Tensor):
+        out = torch.zeros(self.user_ratings.shape[2], dtype=torch.float32)
+        top = top.long()
+        mid = mid.long()
+        sim = sim.long()
+        for i, r in enumerate(top):
+            out[r] += top_rates[i]*probs[0]
+        for i, r in enumerate(mid):
+            out[r] += mid_rates[i]*probs[1]
+        for i, r in enumerate(sim):
+            out[r] += sim_rates[i]*probs[2]
+        recs = self.__get_rec_list(out.reshape((1, -1)))
+        return recs
 
     def generate_training_data(self, X: torch.Tensor, true_ratings: torch.Tensor, exclusive = True):
         assert not torch.is_grad_enabled()
@@ -114,16 +79,21 @@ class EnsembleModel(nn.Module):
         top_preds = self.small_dec(self.small_prior(X)[0])[0]
         top_preds = self.__map_subset_preds(top_preds, self.top_map)
         top_preds = top_preds * mask if mask is not None else top_preds
-        top_preds = self.__get_rec_list(top_preds)
+        top_list = self.__get_rec_list(top_preds)
+        top_rates = top_preds.gather(1, top_list)
+        del top_preds
         mid_preds = self.mid_dec(self.mid_prior(X)[0])[0]
         mid_preds = self.__map_subset_preds(mid_preds, self.mid_map)
         mid_preds = mid_preds * mask if mask is not None else mid_preds
-        mid_preds = self.__get_rec_list(mid_preds)
+        mid_list = self.__get_rec_list(mid_preds)
+        mid_rates = mid_preds.gather(1, mid_list)
+        del mid_preds
         k_preds = recommend_personality(X.cpu().numpy(), self.user_ratings.squeeze(), self.user_personalities.squeeze())
-        k_preds = self.__get_rec_list(torch.from_numpy(k_preds))
-        list
+        k_preds = torch.from_numpy(k_preds)
+        k_list = self.__get_rec_list(k_preds)
+        k_rates = k_preds.gather(1, k_list)
         probs = self.mapper(X)
-        data = torch.stack((top_preds, mid_preds, k_preds), dim=1)
+        data = torch.stack((top_list, mid_list, k_list, top_rates, mid_rates, k_rates), dim=1)
         out = torch.empty((X.shape[0], self.k))
         for i, e in enumerate(data):
             out[i] = self.__fuse_rec_lists(*e, probs[i])

train_ensemble.py
@@ -43,7 +43,7 @@ def load_ensemble_model():
     # Data loading
     X_train, X_test, p_train, p_test = load_ratings_with_personality_traits(path='data/personality-isf2018/', valfrac=0.1, transpose=False, feature_classification=True)
     _, _, _, _, top_indices = load_top_movies_with_personality_traits(path='data/personality-isf2018/', valfrac=0.1, transpose=False, feature_classification=True)
-    _, _, _, _, mid_indices = load_mid_movies_with_personality_traits(path='data/personality-isf2018/', valfrac=0.1, transpose=False, feature_classification=True, n=250)
+    _, _, _, _, mid_indices = load_mid_movies_with_personality_traits(path='data/personality-isf2018/', valfrac=0.1, transpose=False, feature_classification=True, n=0)
     train_mask = torch.greater_equal(torch.from_numpy(X_train), 0.1).float().squeeze()
     test_mask = torch.greater_equal(torch.from_numpy(X_test), 0.1).float().squeeze()
     # Model loading

train_mid_autoencoder.py
@@ -11,7 +11,7 @@ def main():
     seed = int(time.time())
     torch.seed()
     train_data, validation_data, _, _, _ = load_mid_movies_with_personality_traits(
-        path='data/personality-isf2018/', valfrac=0.1, transpose=False, feature_classification=True, n=250)
+        path='data/personality-isf2018/', valfrac=0.1, transpose=False, feature_classification=True, n=0)
     device = get_device()
     train_data = torch.from_numpy(train_data).to(device).squeeze()
     validation_data = torch.from_numpy(validation_data).to(device).squeeze()
@@ -21,7 +21,7 @@ def main():
     print(f'Training mask: {train_mask.shape}, validation mask shape: {validation_mask.shape}')
     print(f'Device: {device}')
     output_every=50
-    epochs = 100
+    epochs = 500
     epochs = output_every * epochs
     model = train_model(
             epochs,
@@ -31,8 +31,8 @@ def main():
             validation_mask,
             kernel=gaussian_kernel,
             activation=torch.nn.Sigmoid(),
-            lambda_o=5e-8,
-            lambda_2=3e-6,
+            lambda_o=7e-7,
+            lambda_2=5e-5,
             history_size=5,
             output_every=50,
             hidden_dims=400,

train_mid_prior.py
@@ -12,7 +12,7 @@ from priors.mid_prior import train_mid_prior
 def main():
     seed = int(time.time())
     train_data, test_data, X_train, X_test, _ = load_mid_movies_with_personality_traits(
-        path='data/personality-isf2018/', valfrac=0.1, transpose=False, feature_classification=True, n=250)
+        path='data/personality-isf2018/', valfrac=0.1, transpose=False, feature_classification=True, n=0)
     device = get_device()
     train_data = torch.from_numpy(train_data).to(device).squeeze()
     test_data = torch.from_numpy(test_data).to(device).squeeze()